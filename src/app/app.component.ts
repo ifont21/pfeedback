30163491bf6b0c6f09a2dfe0e33d870b94dc77e7
Ignacio Fontalvo <ignacioa@backbase.com>
2021-11-06 22:02:28 +0100

    feat: adding showcase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AppShowCaseModule } from './app-showcase/app-showcase.component';
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'product-feedback';
 }
+
+@NgModule({
+  imports: [BrowserModule, AppShowCaseModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
